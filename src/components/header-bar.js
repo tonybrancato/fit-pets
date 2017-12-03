6352e711d9470f09726f7a02f6847df75aeae4b7
Tony Brancato <tonybrancato1@gmail.com>
2017-12-03 11:44:45 -0500

    removed logout button

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {setCurrentUser, setAuthToken} from '../actions/auth';
-import {clearAuthToken} from '../local-storage';
+
 
 export class HeaderBar extends React.Component {
-    logOut() {
-        this.props.dispatch(setCurrentUser(null));
-        this.props.dispatch(setAuthToken(null));
-        clearAuthToken();
-    }
 
     render() {
         // Only render the log out button if we are logged in
-        let logOutButton;
-        if (this.props.loggedIn) {
-            logOutButton = (
-                <button onClick={() => this.logOut()}>Log out</button>
-            );
-        }
+
         return (
             <div className="header-bar">
                 <h1>Pet Fit</h1>
-                {logOutButton}
             </div>
         );
     }
