aebffa5c285d58fe8b5f974d498727c3c82446f6
Tony Brancato <tonybrancato1@gmail.com>
2017-12-03 11:45:07 -0500

    Added styling and logout button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,40 @@
 import React from 'react';
-// import Connect from 'react-redux';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {setCurrentUser, setAuthToken} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
 
 import './navbar.css';
 
-export default function NavBar() {
-    return (
-        <nav className="nav">
-            <Link to="/register">Home</Link>
-        </nav>
-    );
-};
+export class NavBar extends React.Component {
+    logOut() {
+        this.props.dispatch(setCurrentUser(null));
+        this.props.dispatch(setAuthToken(null));
+        clearAuthToken();
+    }
+
+    render() {
+        // Only render the log out button if we are logged in
+        let logOutButton;
+        if (this.props.loggedIn) {
+            logOutButton = (
+                <button className="nav-btn" onClick={() => this.logOut()}>Log out</button>
+            );
+        }
+        return (
+            <div className="nav">
+                <nav className="nav">
+                    <Link className="nav-btn" to="/register"><button>Home</button></Link>
+                    {logOutButton}
+                </nav>              
+            </div>
+        );
+    }  
+}
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(NavBar);
+
