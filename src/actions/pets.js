9c22f234b7e4124b67a0faeb44452afb6ae1f6d9
Tony Brancato <tonybrancato1@gmail.com>
2017-12-10 15:08:50 -0500

    Action to add pets to DB

---
+++ src/actions/pets.js
@@ -0,0 +1,43 @@
+import {SubmissionError} from 'redux-form';
+
+import {API_BASE_URL} from '../config';
+import {normalizeResponseErrors} from './utils';
+
+export const FETCH_PROTECTED_DATA_SUCCESS = 'FETCH_PROTECTED_DATA_SUCCESS';
+export const fetchProtectedDataSuccess = data => ({
+    type: FETCH_PROTECTED_DATA_SUCCESS,
+    data
+});
+
+export const FETCH_PROTECTED_DATA_ERROR = 'FETCH_PROTECTED_DATA_ERROR';
+export const fetchProtectedDataError = error => ({
+    type: FETCH_PROTECTED_DATA_ERROR,
+    error
+});
+
+export const addPet = pet => (dispatch, getState) => {
+  const authToken = getState().auth.authToken;
+  console.log(pet);
+    return fetch(`${API_BASE_URL}/pets`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+             authorization: `Bearer ${authToken}`
+        },
+        body: JSON.stringify(pet)
+    })
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .then(({data}) => dispatch(fetchProtectedDataSuccess(data)))        
+        .catch(err => {
+            const {reason, message, location} = err;
+            if (reason === 'ValidationError') {
+                // Convert ValidationErrors into SubmissionErrors for Redux Form
+                return Promise.reject(
+                    new SubmissionError({
+                        [location]: message
+                    })
+                );
+            }
+        });
+};
