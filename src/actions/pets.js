6bc7a44b8b22542ec82c2b32f9aa03b7cc99e0c5
Tony Brancato <tonybrancato1@gmail.com>
2017-12-13 21:19:14 -0500

    getPets actions

--- src/actions/pets.js
+++ src/actions/pets.js
@@ -15,6 +15,35 @@ export const fetchProtectedDataError = error => ({
     error
 });
 
+export const getPets = pets => (dispatch, getState) => {
+  const authToken = getState().auth.authToken;
+  const USER = getState().auth.currentUser.id;
+  console.log(USER);  
+  console.log(pets);
+    return fetch(`${API_BASE_URL}/pets/${USER}`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${authToken}`
+      },
+      body: JSON.stringify(pets)
+    })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(({data}) => dispatch(fetchProtectedDataSuccess(data)))        
+    .catch(err => {
+        const {reason, message, location} = err;
+        if (reason === 'ValidationError') {
+            // Convert ValidationErrors into SubmissionErrors for Redux Form
+            return Promise.reject(
+                new SubmissionError({
+                    [location]: message
+                })
+            );
+        }
+    });
+}
+
 export const addPet = pet => (dispatch, getState) => {
   const authToken = getState().auth.authToken;
   console.log(pet);
