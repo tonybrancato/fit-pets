995a13620d5f51f8b1a9f746bc0c6ce9aba14b3e
Tony Brancato <tonybrancato1@gmail.com>
2017-12-23 10:45:07 -0500

    simplified for readability

--- src/components/pet.js
+++ src/components/pet.js
@@ -15,55 +15,56 @@ export class Pet extends React.Component {
   }
 
   render() {
-    
+    const pets = this.props.pets;
+    const accordionItems = (pets).map((pet, i) => {
+      console.log(pets);
+      return (
+        <AccordionItem
+          key={i} 
+          className="pet-box" 
+          bodyClassName="pet-box-expanded" 
+          duration={400}
+          title={`${pet.name}`} 
+          expanded={pet === 1}>
+            <div>
+              <h3>{`${pet.name}'s Current Weight: ${pet.lastWeight} pounds`}</h3>
+              <h3>{`Age: ${pet.age}s`}</h3>
+              <LineExample data={{
+                labels: pet.weightDate,
+                datasets: [
+                  {
+                    label: pet.name,
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: pet.weight
+                  }
+                ]
+              }} />
+            </div>
+        </AccordionItem>
+      );
+  })
     return (
       <Accordion 
         key={this.props.id}
         allowMultiple={true} 
         className="pet-parent">         
-          {(this.props.pets).map((pet, i) => {
-            console.log(this.props.pets);
-            return (
-              <AccordionItem
-                key={i} 
-                className="pet-box" 
-                bodyClassName="pet-box-expanded" 
-                duration={400}
-                title={`${this.props.pets[i].name}`} 
-                expanded={pet === 1}>
-                  <div>
-                    <h3>{`${this.props.pets[i].name}'s Current Weight: ${this.props.pets[i].lastWeight} pounds`}</h3>
-                    <h3>{`Age: ${this.props.pets[i].age}s`}</h3>
-                    <LineExample data={{
-                      labels: this.props.pets[i].weightDate,
-                      datasets: [
-                        {
-                          label: this.props.pets[i].name,
-                          fill: false,
-                          lineTension: 0.1,
-                          backgroundColor: 'rgba(75,192,192,0.4)',
-                          borderColor: 'rgba(75,192,192,1)',
-                          borderCapStyle: 'butt',
-                          borderDash: [],
-                          borderDashOffset: 0.0,
-                          borderJoinStyle: 'miter',
-                          pointBorderColor: 'rgba(75,192,192,1)',
-                          pointBackgroundColor: '#fff',
-                          pointBorderWidth: 1,
-                          pointHoverRadius: 5,
-                          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                          pointHoverBorderColor: 'rgba(220,220,220,1)',
-                          pointHoverBorderWidth: 2,
-                          pointRadius: 1,
-                          pointHitRadius: 10,
-                          data: this.props.pets[i].weight
-                        }
-                      ]
-                    }} />
-                  </div>
-              </AccordionItem>
-            );
-        })}
+        {accordionItems}
       </Accordion>
     );
   }
